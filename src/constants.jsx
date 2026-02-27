e098020755e8884c6e577d1220cea36e0a33c4a6
ShehabEldeenAyman <shehab.ayman@vito.be>
2026-02-27 17:09:05 +0100

    Update constants.jsx

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,2 +1,2 @@
-//export const base_url = 'http://localhost:3000/';
-export const base_url = 'https://ed-pretelephone-unpleasantly.ngrok-free.dev/';
+export const base_url = 'http://localhost:3000/';
+//export const base_url = 'https://ed-pretelephone-unpleasantly.ngrok-free.dev/';
